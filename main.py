39366fc35de9ba193c36c3f08eab35053171aaa4
Lachlan Connan <lachlan@connan.org>
2019-10-02 19:52:00 +0800

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Add imports here (classes and stuff)
+#import ships
+
+player = input("What is your name?\n> ")
+print("Hi " + player + "!\n\n")
+
+#print(ships.s1.name + " is approaching!\n")
+print("What would you like to do?\n1. Attack\n2. Flee\n")
+choice = int(input("> "))
+
