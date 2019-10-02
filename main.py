93c8fec4aa6876e2482435ca915b0d8b6c08d8a2
Lachlan Connan <lachlan@connan.org>
2019-10-02 21:00:42 +0800

    added weapon choice in main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print("Hi " + player + "!")
 ships.playerShip.name = input("What would you like to name your ship?\n> ")
 print(ships.playerShip.name + " it is!")
 
+playerShip = ships.playerShip
+
 print(ships.s1.name + " is approaching!\n")
 print("What would you like to do?\n1. Attack\n2. Flee\n")
 choice = input("> ")    
@@ -18,13 +20,32 @@ if choice == "1":
     while True:
         choice = input("1. Fire\n> ")
         if choice == "1":
-            ships.s1.hp -= ships.playerShip.weapons[0].damage
-            print("You delt " + str(ships.playerShip.weapons[0].damage) + " to enemy ship")
-            print(ships.s1.name + " is on " + str(ships.s1.hp) + " hp!")
+            selectedWeapon = playerShip.weapons[0]
+            loop = 1
+            print("")
 
-            if ships.s1.hp <= 0 or ships.playerShip.hp <= 0:
-                print("\nYou destroyed the enemy ship!")
-                break;
+            for weapon in playerShip.weapons:
+                print(str(loop) + ". " + weapon.name)
+                
+                #ships.s1.hp -= playerShip.weapons[0].damage
+                #print("You delt " + str(playerShip.weapons[0].damage) + " to enemy ship")
+                #print(ships.s1.name + " is on " + str(ships.s1.hp) + " hp!")
+
+                #if ships.s1.hp <= 0 or playerShip.hp <= 0:
+                #    print("\nYou destroyed the enemy ship!")
+                #    break;
+
+                loop += 1
+
+            choice = input("Choose a weapon:\n> ")
+            loop = 1
+            for weapon in playerShip.weapons:
+                if choice == str(loop):
+                    selectedWeapon = playerShip.weapons[loop - 1]
+
+                loop += 1
+
+            print(selectedWeapon.name)
         else:
             print("Invalid input")
         
--- weapons.py
+++ weapons.py
@@ -1,8 +1,9 @@
 class Weapon:
 	def __init__(self, name, damage, cooldown, accuracy):
+		self.name = name
 		self.damage = damage
 		self.cooldown = cooldown
 		self.accuracy = accuracy
 
-w1 = Weapon("lasers",10,10,10)
-w2 = Weapon("missile",5,5,5)
+w1 = Weapon("lasers", 10, 10, 10)
+w2 = Weapon("missile", 5, 5, 5)
