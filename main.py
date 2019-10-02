f7c6c18236259d4cf9b31b0bcdec70f71fed3c9d
Lachlan Connan <lachlan@connan.org>
2019-10-02 21:37:29 +0800

    can now fire selected weapon in main.py

--- main.py
+++ main.py
@@ -16,39 +16,44 @@ print("What would you like to do?\n1. Attack\n2. Flee\n")
 choice = input("> ")    
 
 if choice == "1":
-    print("You begun combat!")
+    print("You begun combat!\n")
     while True:
-        choice = input("1. Fire\n> ")
+        choice = input("Select an option:\n1. Fire\n> ")
         if choice == "1":
-            selectedWeapon = playerShip.weapons[0]
-            loop = 1
-            print("")
+            while True:
+                selectedWeapon = "null"
+                loop = 1
+                print("\nChoose a weapon:")
 
-            for weapon in playerShip.weapons:
-                print(str(loop) + ". " + weapon.name)
-                
-                #ships.s1.hp -= playerShip.weapons[0].damage
-                #print("You delt " + str(playerShip.weapons[0].damage) + " to enemy ship")
-                #print(ships.s1.name + " is on " + str(ships.s1.hp) + " hp!")
+                for weapon in playerShip.weapons:
+                    print(str(loop) + ". " + weapon.name)
+                    loop += 1
 
-                #if ships.s1.hp <= 0 or playerShip.hp <= 0:
-                #    print("\nYou destroyed the enemy ship!")
-                #    break;
+                loop = 1
+                choice = input("> ")
+                for weapon in playerShip.weapons:
+                    if choice == str(loop):
+                        selectedWeapon = playerShip.weapons[loop - 1]
+                        break
 
-                loop += 1
+                    if int(choice) > len(playerShip.weapons):
+                        print("Invalid option")
+                        break
 
-            choice = input("Choose a weapon:\n> ")
-            loop = 1
-            for weapon in playerShip.weapons:
-                if choice == str(loop):
-                    selectedWeapon = playerShip.weapons[loop - 1]
+                    loop += 1
+                if selectedWeapon != "null":
+                    break
+    
+            print("\nFiring " + selectedWeapon.name + "...")
+            ships.s1.hp -= selectedWeapon.damage
+            print("You delt " + str(selectedWeapon.damage) + " to enemy ship")
+            print(ships.s1.name + " is on " + str(ships.s1.hp) + " hp!\n")
 
-                loop += 1
-
-            print(selectedWeapon.name)
+            if ships.s1.hp <= 0 or playerShip.hp <= 0:
+                print("\nYou destroyed the enemy ship!")
+                break;
         else:
             print("Invalid input")
-        
 elif choice == "2":
     print("You fled")
 else:
