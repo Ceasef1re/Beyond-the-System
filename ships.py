172e825406512257284ed15d9b038b3f11366e36
Ceasef1re <zingrazor@gmail.com>
2019-10-02 20:23:04 +0800

    changes to ships

--- ships.py
+++ ships.py
@@ -1,9 +1,11 @@
+import Weapons
 class Ship:
-  def __init__(self, name, hp, weapons, defences):
+  def __init__(self, name, hp, wepNumber, weapons, defences):
     self.name = name
     self.hp = hp
-    self.weapons = weapons
+    self.wepNumber = wepNumber
+	self.wepons = weapons
     self.defences = defences
 
-s1 = Ship("player", 100, 2, "medium")
-s2 = Ship("boop", 100, 2, "medium")
+playerShip = Ship("", 100, 2, [weapons.w1, weapons.w2] "medium")
+s1 = Ship("boop", 100, 2, [weapons.w1, weapons.w2] "medium")
