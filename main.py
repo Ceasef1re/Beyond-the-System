54cbe6d01d683579fb27ca0c09a97db21766e828
Ceasef1re <zingrazor@gmail.com>
2019-10-06 17:29:51 +0800

    name tables for ships

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print("Hi " + player + "!")
 ships.playerShip.name = input("What would you like to name your ship?\n> ")
 print(ships.playerShip.name + " it is!")
 
-print(ships.s1.name + " is approaching!\n")
+print(ships.randShip.name + " is approaching!\n")
 print("What would you like to do?\n1. Attack\n2. Flee\n")
 choice = input("> ")    
 
--- ships.py
+++ ships.py
@@ -10,28 +10,31 @@ class Ship:
         self.weapons = weapons
         self.armour = armour
 
+name1 = ["Blazing ", "Mercenary ", "Battle ", "Resilient ",
+         "Iron-Side ", "Crimson ", "Final ", "Steel ",
+         "Black ", "New ", "Galactic ", "Invincible ",
+         "Opal ", "Silent ", "Maiden ", "Fire ",
+         "Close ", "Wailing ", "Dark ", "Ark "]
+
+name2 = ["Thorn", "Star", "Mercury", "Ambasador",
+         "Valkyrie", "Cruiser", "Frontier", "Aurora",
+         "Cloud", "Hope", "Core", "World",
+         "Titan", "Wyvern", "Voyager", "Fury",
+         "Encounter", "Talon", "Wind", "Phoenix"]
+
 def createRandShip():
-        randShip = Ship("random",
+        randShip = Ship("The " + random.choice(name1) + random.choice(name2),
                     random.randint(1,100),
                     random.randint(1,100),
                     random.randint(1,10),
-                    for i in range randShip.weaponNum:
-                        random.choice(weapons.availableWeapons),
+                    random.choice(weapons.availableWeapons),
                     random.choice(["light", "medium", "heavy"]))
         return randShip
 
-playerShip = Ship("",
-                  100,
-                  100,
-                  2,
-                  [weapons.availableWeapons[0], weapons.availableWeapons[1]],
-                  "medium")
+playerShip = Ship("", 100, 100,
+                  2, [weapons.availableWeapons[0], weapons.availableWeapons[1]], "medium")
 
-s1 = Ship("boop",
-          100,
-          100,
-          2,
-          [weapons.availableWeapons[0], weapons.availableWeapons[0]],
-          "medium")
+s1 = Ship("boop", 100, 100,
+          2, [weapons.availableWeapons[0], weapons.availableWeapons[0]], "medium")
 
 randShip = createRandShip()
